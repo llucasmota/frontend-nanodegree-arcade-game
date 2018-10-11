1cd73ebe8e0cf682104949b12d30b34b5acb0f7d
llucasmota <lucas.o.mota@gmail.com>
2018-10-10 22:56:56 -0300

    Criacao de classe Generic para suporte as classes Player e Enemy e ainda criacao da classe Enemy

--- js/app.js
+++ js/app.js
@@ -1,26 +1,43 @@
+var spriteEnemy = "images/enemy-bug.png";
+class Generic {
+    constructor(x, y, sprite) {
+        this.x = x;
+        this.y = y;
+        this.sprite = sprite;
+        this.speed = speed;
+    }
+}
 // Inimigos que nosso jogador deve evitar
-var Enemy = function() {
+
+class Enemy extends Generic {
     // As variáveis aplicadas a nossas instâncias entram aqui.
     // Fornecemos uma a você para que possa começcar.
 
     // A imagem/sprite de nossos inimigos, isso usa um
     // ajudante que é fornecido para carregar imagens
     // com facilidade.
-    this.sprite = 'images/enemy-bug.png';
-};
+    constructor(x, y, speed, sprite) {
+        super(x, y, sprite);
+        this.speed = speed;
+    }
+    // Desenhe o inimigo na tela, método exigido pelo jogo
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
-// Atualize a posição do inimigo, método exigido pelo jogo
-// Parâmetro: dt, um delta de tempo entre ticks
-Enemy.prototype.update = function(dt) {
+    }
+    // Atualize a posição do inimigo, método exigido pelo jogo
+    // Parâmetro: dt, um delta de tempo entre ticks
+    update(dt) {
+        this.x += speed * dt;
+    }
     // Você deve multiplicar qualquer movimento pelo parâmetro
     // dt, o que garantirá que o jogo rode na mesma velocidade
     // em qualquer computador.
-};
+}
 
-// Desenhe o inimigo na tela, método exigido pelo jogo
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+
+
+
 
 // Agora, escreva sua própria classe de jogador
 // Esta classe exige um método update(), 
@@ -35,7 +52,7 @@ Enemy.prototype.render = function() {
 
 // Isto reconhece cliques em teclas e envia as chaves para seu
 // jogador. método handleInput(). Não é preciso mudar nada.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
